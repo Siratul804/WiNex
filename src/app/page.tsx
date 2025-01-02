46e066865b917e6fdcc911a720460ef0dae4cf67
Siratul Islam <islamsiratul@gmail.com>
2025-01-02 15:14:53 +0600

    feat: added connection with wix-stdio

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,34 @@
-import Slider from "./components/Slider";
+// "use client";
 
-const HomePage = () => {
+import { useContext, useEffect } from "react";
+import Slider from "../components/Slider";
+import { WixClientContext } from "@/context/wixContext";
+import { useWixClient } from "@/hooks/useWixClient";
+import { wixClientServer } from "@/lib/wixClientServer";
+
+const HomePage = async () => {
+  //  TEST (FETCHING ON THE CLIENT COMPONENT)
+
+  // const wixClient = useWixClient();
+
+  // useEffect(() => {
+  //   const getProducts = async () => {
+  //     const res = await wixClient.products.queryProducts().find();
+
+  //     console.log(res);
+  //   };
+
+  //   getProducts();
+  // }, [wixClient]);
+
+  // TEST (FETCHING ON THE SERVER COMPONENT)
+
+  const wixClient = await wixClientServer();
+
+  const res = await wixClient.products.queryProducts().find();
+
+  console.log(res);
+
   return (
     <>
       <Slider />
