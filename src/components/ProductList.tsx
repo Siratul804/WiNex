8f897204fe55a78823f1b99374fb39da8e42d3a8
Siratul Islam <islamsiratul@gmail.com>
2025-01-05 14:02:53 +0600

    feat: add filter,pagination

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -3,7 +3,7 @@ import { products } from "@wix/stores";
 import Image from "next/image";
 import Link from "next/link";
 import DOMPurify from "isomorphic-dompurify";
-// import Pagination from "./Pagination";
+import Pagination from "./Pagination";
 
 const PRODUCT_PER_PAGE = 8;
 
@@ -18,45 +18,36 @@ const ProductList = async ({
 }) => {
   const wixClient = await wixClientServer();
 
-  // const productQuery = wixClient.products
-  //   .queryProducts()
-  //   .startsWith("name", searchParams?.name || "")
-  //   .eq("collectionIds", categoryId)
-  //   .hasSome(
-  //     "productType",
-  //     searchParams?.type ? [searchParams.type] : ["physical", "digital"]
-  //   )
-  //   .gt("priceData.price", searchParams?.min || 0)
-  //   .lt("priceData.price", searchParams?.max || 999999)
-  //   .limit(limit || PRODUCT_PER_PAGE)
-  //   .skip(
-  //     searchParams?.page
-  //       ? parseInt(searchParams.page) * (limit || PRODUCT_PER_PAGE)
-  //       : 0
-  //   );
+  let productQuery = wixClient.products
+    .queryProducts()
+    .startsWith("name", searchParams?.name || "")
+    .eq("collectionIds", categoryId)
+    .hasSome(
+      "productType",
+      searchParams?.type ? [searchParams.type] : ["physical", "digital"]
+    )
+    .gt("priceData.price", searchParams?.min || 0)
+    .lt("priceData.price", searchParams?.max || 999999)
+    .limit(limit || PRODUCT_PER_PAGE)
+    .skip(
+      searchParams?.page
+        ? parseInt(searchParams.page) * (limit || PRODUCT_PER_PAGE)
+        : 0
+    );
   // .find();
 
-  // if (searchParams?.sort) {
-  //   const [sortType, sortBy] = searchParams.sort.split(" ");
+  if (searchParams?.sort) {
+    const [sortType, sortBy] = searchParams.sort.split(" ");
 
-  //   if (sortType === "asc") {
-  //     productQuery.ascending(sortBy);
-  //   }
-  //   if (sortType === "desc") {
-  //     productQuery.descending(sortBy);
-  //   }
-  // }
+    if (sortType === "asc") {
+      productQuery = productQuery.ascending(sortBy); // Reassign the query
+    }
+    if (sortType === "desc") {
+      productQuery = productQuery.descending(sortBy); // Reassign the query
+    }
+  }
 
-  // const res = await productQuery.find();
-
-  console.log(categoryId);
-  const res = await wixClient.products
-    .queryProducts()
-    .eq("collectionIds", categoryId)
-    .limit(limit || PRODUCT_PER_PAGE)
-    .find();
-
-  console.log(res.items[0]);
+  const res = await productQuery.find();
 
   return (
     <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
@@ -105,6 +96,13 @@ const ProductList = async ({
           </button>
         </Link>
       ))}
+      {searchParams?.cat || searchParams?.name ? (
+        <Pagination
+          currentPage={res.currentPage || 0}
+          hasPrev={res.hasPrev()}
+          hasNext={res.hasNext()}
+        />
+      ) : null}
     </div>
   );
 };
